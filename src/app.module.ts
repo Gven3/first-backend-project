e2b3dee349761b2292cacce09b75912d85d6786a
Gven3 <giogvenetadze13@gmail.com>
2023-08-06 05:16:35 -0700

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,19 +6,19 @@ import { User } from './typeorm/entities/User';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'first-backend-project-production-0211.up.railway.app',
-      port: 80,
-      username: 'root',
-      password: 'gioekoilia1!',
-      database: 'quizdb',
-      entities: [User],
-      synchronize: true,
-    }),
-    UsersModule,
-  ],
+  // imports: [
+  //   TypeOrmModule.forRoot({
+  //     type: 'mysql',
+  //     host: '0.0.0.0',
+  //     port: 80,
+  //     username: 'root',
+  //     password: 'gioekoilia1!',
+  //     database: 'quizdb',
+  //     entities: [User],
+  //     synchronize: true,
+  //   }),
+  //   UsersModule,
+  // ],
   controllers: [AppController],
   providers: [AppService],
 })
