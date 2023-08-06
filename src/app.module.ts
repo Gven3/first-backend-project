cda03e826fa90246f63d53df50fb1b321e6e5ed2
Gven3 <giogvenetadze13@gmail.com>
2023-08-06 05:22:45 -0700

    final fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,22 +3,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './typeorm/entities/User';
-import { UsersModule } from './users/users.module';
 
 @Module({
-  // imports: [
-  //   TypeOrmModule.forRoot({
-  //     type: 'mysql',
-  //     host: '0.0.0.0',
-  //     port: 80,
-  //     username: 'root',
-  //     password: 'gioekoilia1!',
-  //     database: 'quizdb',
-  //     entities: [User],
-  //     synchronize: true,
-  //   }),
-  //   UsersModule,
-  // ],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'gioekoilia1!',
+      database: 'quizdb',
+      entities: [User],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
