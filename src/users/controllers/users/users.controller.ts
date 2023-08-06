28636c3991c6d328bbf7b60bb79067496dbc064e
Gven3 <giogvenetadze13@gmail.com>
2023-08-06 07:39:14 -0700

    login

--- src/users/controllers/users/users.controller.ts
+++ src/users/controllers/users/users.controller.ts
@@ -1,6 +1,8 @@
-import { Controller,Post,Body, Get } from '@nestjs/common';
+import { Controller,Post,Body, Get, BadRequestException } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
+import { User } from 'src/typeorm/entities/User';
 import { CreateUserDto } from 'src/users/dtos/userCreate.dto';
+import { userLogin } from 'src/users/dtos/userLogin.dto';
 import { UsersService } from 'src/users/services/users/users.service';
 
 @Controller('users')
@@ -8,14 +10,21 @@ export class UsersController {
 
     constructor(private userService: UsersService){}
 
-
-    @Get("register")
-    getReg(){
-    return {email: "asfsafasfsa"}
-    }
     @Post("register")
-    async createUser(@Body() userData: CreateUserDto){
+    async register(@Body() userData: CreateUserDto){
         const hashedPassword = await bcrypt.hash(userData.password, 10);
         return this.userService.createUser({...userData, createdAt: new Date(), password: hashedPassword})
+    }
+    @Get("login")
+    async login(@Body() userData: userLogin) {
+        const {email} = userData
+        const user = await this.userService.findOne({email})
+        if(!user){
+            throw new BadRequestException("invalid credentials")
+        }
+        if(!await bcrypt.compare(userData.password , user.password)){
+            throw new BadRequestException("password is wrong")
+        }
+        return user
     }
 }
--- src/users/services/users/users.service.ts
+++ src/users/services/users/users.service.ts
@@ -2,14 +2,18 @@ import { Injectable } from '@nestjs/common';
 import {InjectRepository} from "@nestjs/typeorm"
 import { User } from 'src/typeorm/entities/User';
 import { CreateUserDto } from 'src/users/dtos/userCreate.dto';
-import {Repository} from "typeorm"
+import {Repository,FindOneOptions} from "typeorm"
 
 @Injectable()
 export class UsersService {
     constructor(@InjectRepository(User) private userRepository: Repository<User>){}
 
     createUser(userData:CreateUserDto){
-        const user= this.userRepository.create(userData)
+        const user = this.userRepository.create(userData)
         return this.userRepository.save(user)
+    }
+
+    findOne(condition : any) : Promise<User>{
+        return this.userRepository.findOne(condition)
     }
 }
