97e4f222c0af53df15458c5322b476ada3abdc93
Gven3 <giogvenetadze13@gmail.com>
2023-08-07 00:06:41 -0700

    added jwt token to cookies

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as process from "process"
+import * as process from 'process';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    credentials: true,
+  });
   await app.listen(process.env.PORT);
 }
 bootstrap();
--- src/users/controllers/users/users.controller.ts
+++ src/users/controllers/users/users.controller.ts
@@ -1,29 +1,49 @@
-import { Controller,Post,Body, Get, BadRequestException } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  BadRequestException,
+  Res,
+} from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
-import { User } from 'src/typeorm/entities/User';
 import { CreateUserDto } from 'src/users/dtos/userCreate.dto';
 import { userLogin } from 'src/users/dtos/userLogin.dto';
 import { UsersService } from 'src/users/services/users/users.service';
+import { JwtService } from '@nestjs/jwt';
+import { Response } from 'express';
 
 @Controller('users')
 export class UsersController {
+  constructor(
+    private userService: UsersService,
+    private jwtService: JwtService,
+  ) {}
 
-    constructor(private userService: UsersService){}
-
-    @Post("register")
-    async register(@Body() userData: CreateUserDto){
-        const hashedPassword = await bcrypt.hash(userData.password, 10);
-        return this.userService.createUser({...userData, createdAt: new Date(), password: hashedPassword})
+  @Post('register')
+  async register(@Body() userData: CreateUserDto) {
+    const hashedPassword = await bcrypt.hash(userData.password, 10);
+    return this.userService.createUser({
+      ...userData,
+      createdAt: new Date(),
+      password: hashedPassword,
+    });
+  }
+  @Post('login')
+  async login(
+    @Body() userData: userLogin,
+    @Res({ passthrough: true }) response: Response,
+  ) {
+    const user = await this.userService.findOne(userData.email);
+    if (!user) {
+      throw new BadRequestException('email is wrong');
     }
-    @Post("login")
-    async login(@Body() userData: userLogin) {
-        const user = await this.userService.findOne(userData.email)
-        if(!user){
-            throw new BadRequestException("invalid credentials")
-        }
-        if(!await bcrypt.compare(userData.password , user.password)){
-            throw new BadRequestException("password is wrong")
-        }
-        return user
+    if (!(await bcrypt.compare(userData.password, user.password))) {
+      throw new BadRequestException('password is wrong');
     }
+    const jwt = await this.jwtService.signAsync({ id: user.id });
+    response.cookie('jwt', jwt, { httpOnly: true });
+    const { password, ...result } = user;
+    return result;
+  }
 }
